e0bd1d18afb6bc598c95b2d5301fef700706d094
stephenwelch1927 <stephenwelch1927@yahoo.com>
2025-02-25 15:08:49 +0700

    Updated bootstrap card to adjust button feature

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,19 +9,19 @@ const Projects = () => {
     <div>
       <h1>Projects</h1>
       <Card
-        bg={"secondary"}
+        bg={"light"}
         style={{ width: "100%" }}
         className="img1"
       >
-        <Card.Title style={{ color: "white" }}>Global Beer Garden</Card.Title>
+        <Card.Title className="card-title">Global Beer Garden</Card.Title>
         <Card.Img variant="top" src={Global} />
         <Card.Body>
-          <Card.Text style={{ color: "white" }}>
+          <Card.Text>
             This website is a fully functional beer purchasing site, it allows
             the customer to browse and purchase beers from across the globe.
           </Card.Text>
 
-          <Button variant="danger">Global Beer Garden</Button>
+          <Button variant="dark" size="sm">Global Beer Garden</Button>
         </Card.Body>
       </Card>
     </div>
