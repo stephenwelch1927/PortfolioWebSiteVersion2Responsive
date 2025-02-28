e1d377e15358b97337f309ac08832df85abd5d8c
stephenwelch1927 <stephenwelch1927@yahoo.com>
2025-02-28 08:56:39 +0700

    Updated button also added in Recipe App card to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Global from "../Img/globalBeer1.png";
 import Holiday from "../Img/holiday1.png";
+import Food from "../Img/Food_and_Drink_Guide.png";
 import Project from "./Projects.css";
 
 const Projects = () => {
@@ -10,38 +11,49 @@ const Projects = () => {
     <div>
       <h1>Projects</h1>
       <div className="container-display">
-      <Card
-        bg={"light"}
-        style={{ width: "100%" }}
-        className="img1"
-      >
-        <Card.Title className="card-title">Global Beer Garden</Card.Title>
-        <Card.Img variant="top" src={Global} />
-        <Card.Body>
-          <Card.Text>
-            This website is a fully functional beer purchasing site, it allows
-            the customer to browse and purchase beers from across the globe.
-          </Card.Text>
+        <Card bg={"light"} style={{ width: "100%" }} className="img1">
+          <Card.Title className="card-title">Global Beer Garden</Card.Title>
+          <Card.Img variant="top" src={Global} />
+          <Card.Body>
+            <Card.Text>
+              This website is a fully functional beer purchasing site, it allows
+              the customer to browse and purchase beers from across the globe.
+            </Card.Text>
 
-          <Button className="btn-style" size="sm">Global Beer Garden</Button>
-        </Card.Body>
-      </Card>
-      <Card
-        bg={"light"}
-        style={{ width: "100%" }}
-        className="img1"
-      >
-        <Card.Title className="card-title">Holiday Experience</Card.Title>
-        <Card.Img variant="top" src={Holiday} />
-        <Card.Body>
-          <Card.Text>
-          Allows the user to update their holiday experiences, users can
-          also read other users holiday experience reviews.
-          </Card.Text>
+            <Button className="btn-style" size="sm">
+              Global Beer Garden
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card bg={"light"} style={{ width: "100%" }} className="img1">
+          <Card.Title className="card-title">Holiday Experience</Card.Title>
+          <Card.Img variant="top" src={Holiday} />
+          <Card.Body>
+            <Card.Text>
+              Allows the user to update their holiday experiences, users can
+              also read other users holiday experience reviews.
+            </Card.Text>
 
-          <Button className="btn-style" size="sm">Holiday Experience</Button>
-        </Card.Body>
-      </Card>
+            <Button className="btn-style" size="sm">
+              Holiday Experience
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card bg={"light"} style={{ width: "100%" }} className="img1">
+          <Card.Title className="card-title">Recipe Mobile App</Card.Title>
+          <Card.Img variant="top" src={Food} />
+          <Card.Body>
+            <Card.Text>
+              This mobile app allows the users to browse and update recipes. The
+              user will be able to read and post up their favourite recipes.
+            </Card.Text>
+
+            <Button className="btn-style" size="sm">
+              Recipe App
+            </Button>
+          </Card.Body>
+        </Card>
       </div>
     </div>
   );
