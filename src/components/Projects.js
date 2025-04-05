7f1d39f5d4050d5a95c150f92a0be81d8d51a9cf
stephenwelch1927 <stephenwelch1927@yahoo.com>
2025-04-05 16:44:50 +0700

    Added in cards to the project section which included Markdown Previewer, Reg Ex Parser, Random Quote & Text Simplifier

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,10 @@ import Card from "react-bootstrap/Card";
 import Global from "../Img/globalBeer1.png";
 import Holiday from "../Img/holiday1.png";
 import Food from "../Img/Food_and_Drink_Guide.png";
+import MarkDown from "../Img/markDown.png";
+import RegEx from "../Img/reExConv.png";
+import RandomQuote from "../Img/randomQuote.png";
+import TextSimple from "../Img/textSimplifier.png";
 import Project from "./Projects.css";
 
 const Projects = () => {
@@ -19,7 +23,6 @@ const Projects = () => {
               This website is a fully functional beer purchasing site, it allows
               the customer to browse and purchase beers from across the globe.
             </Card.Text>
-
             <Button className="btn-style" size="sm">
               Global Beer Garden
             </Button>
@@ -33,7 +36,6 @@ const Projects = () => {
               Allows the user to update their holiday experiences, users can
               also read other users holiday experience reviews.
             </Card.Text>
-
             <Button className="btn-style" size="sm">
               Holiday Experience
             </Button>
@@ -48,9 +50,67 @@ const Projects = () => {
               This mobile app allows the users to browse and update recipes. The
               user will be able to read and post up their favourite recipes.
             </Card.Text>
-
             <Button className="btn-style" size="sm">
               Recipe App
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card bg={"light"} style={{ width: "100%" }} className="img1">
+          <Card.Title className="card-title">Markdown Previewer</Card.Title>
+          <Card.Img variant="top" src={MarkDown} />
+          <Card.Body>
+            <Card.Text>
+              App built so the user can transform normal words into markdown
+              language.
+            </Card.Text>
+            <Button className="btn-style" size="sm">
+              Markdown Prev
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card bg={"light"} style={{ width: "100%" }} className="img1">
+          <Card.Title className="card-title">
+            Regular Expression Parser
+          </Card.Title>
+          <Card.Img variant="top" src={RegEx} />
+          <Card.Body>
+            <Card.Text>
+              Using the Python language created a command line Regular
+              expression parser, the user can input a string and then use one of
+              the options to search for the inputted string
+            </Card.Text>
+            <Button className="btn-style" size="sm">
+              Reg Ex Parser
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card bg={"light"} style={{ width: "100%" }} className="img1">
+          <Card.Title className="card-title">Random Quote Generator</Card.Title>
+          <Card.Img variant="top" src={RandomQuote} />
+          <Card.Body>
+            <Card.Text>
+              Created an application that makes an API call to a website, then
+              in turn can generate a random quote.
+            </Card.Text>
+            <Button className="btn-style" size="sm">
+              Random Quote
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card bg={"light"} style={{ width: "100%" }} className="img1">
+          <Card.Title className="card-title">Text Simplifier</Card.Title>
+          <Card.Img variant="top" src={TextSimple} />
+          <Card.Body>
+            <Card.Text>
+            A text Simplifier created using JAVA and making use of its Object Oriented
+            programming capabilities.
+            </Card.Text>
+            <Button className="btn-style" size="sm">
+              Text Simplifier
             </Button>
           </Card.Body>
         </Card>
